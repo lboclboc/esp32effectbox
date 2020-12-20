06bcd2bccaf528ef01fb758f7e043dcb232ccb32
Lars Berntzon <lars.berntzon@cecilia-data.se>
2020-12-20 23:03:48 +0100

    Does not seem to be possible to use APPL clock on both ADC and I2S out

--- main/app_main.c
+++ main/app_main.c
@@ -23,7 +23,7 @@ typedef int16_t sample_t;
 static const char *TAG = "app_main";
 
 //I2S read buffer length
-#define BUFFER_SIZE      		  (256)
+#define BUFFER_SIZE      		  (1024)
 
 #define SAMPLE_RATE 44100
 
@@ -38,23 +38,24 @@ static void tweak_registers();
  * - 26: BCK
  * - 27: Data_Out
  */
-int i2s_init(void)
+int i2s_init_0(void)
 {
      esp_err_t rc;
 
-#if 0 // 6086: Does not fix 1-bit size error.
-     // powerdown APLL/PLLA
-     SET_PERI_REG_MASK(RTC_CNTL_ANA_CONF_REG, RTC_CNTL_PLLA_FORCE_PD_M);
-     CLEAR_PERI_REG_MASK(RTC_CNTL_ANA_CONF_REG, RTC_CNTL_PLLA_FORCE_PU_M);
-
-     // config adc atten and width
-     adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_11db);
-     adc1_config_width(ADC_WIDTH_12Bit);
-
-     // set pattern tbl and invert, might not need, double check
-     SYSCON.saradc_ctrl.sar1_patt_len = 1;
-     SYSCON.saradc_sar1_patt_tab[0] = 0b00001111000011110000111100001111;
-     SYSCON.saradc_ctrl2.sar1_inv = 1;
+#if 0 // ARDUINO CODE
+     i2s_config_t i2s_config = {
+      .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_ADC_BUILT_IN),
+      .sample_rate =  I2S_SAMPLE_RATE,              // The format of the signal using ADC_BUILT_IN
+      .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT, // is fixed at 12bit, stereo, MSB
+      .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+      .communication_format = I2S_COMM_FORMAT_I2S_MSB,
+      .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+      .dma_buf_count = 4,
+      .dma_buf_len = 8,
+      .use_apll = false,
+      .tx_desc_auto_clear = false,
+      .fixed_mclk = 0
+     };
 #endif
 
      // Testlog:
@@ -75,7 +76,7 @@ int i2s_init(void)
         .intr_alloc_flags = 0,
         .dma_buf_count = 2,
         .dma_buf_len = BUFFER_SIZE,
-        .use_apll = 1,
+        .use_apll = false,
 		.fixed_mclk = 0,
      };
 
@@ -85,7 +86,38 @@ int i2s_init(void)
          return rc;
      }
 
-	 // I2S1 DAC Output
+#if 0 // This causes the is2_1 pin outputs to disapear.
+     if ((rc = i2s_set_pin(I2S_NUM_0, NULL)) != ESP_OK) {
+         ESP_LOGE(TAG, "Failed calling i2s_set_pin");
+         return rc;
+     }
+#endif
+
+
+     if ((rc = i2s_set_adc_mode(ADC_UNIT_1, ADC1_CHANNEL_0)) != ESP_OK) {
+     	 ESP_LOGE(TAG, "Failed calling i2s_set_adc_mode");
+         return rc;
+     }
+     // config adc atten and width
+     if ((rc = adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_11db)) != ESP_OK) {
+     	 ESP_LOGE(TAG, "Failed calling adc1_config_channel_atten");
+         return rc;
+     }
+     if ((rc =  adc1_config_width(ADC_WIDTH_12Bit)) != ESP_OK) {
+     	 ESP_LOGE(TAG, "Failed calling adc1_config_width");
+         return rc;
+     }
+
+     i2s_adc_enable(I2S_NUM_0);
+
+     return ESP_OK;
+}
+
+int i2s_init_1()
+{
+     esp_err_t rc;
+
+	 // I2S1 I2S Output
      i2s_config_t i2s1_config = {
         .mode = I2S_MODE_MASTER | I2S_MODE_TX,
         .sample_rate =  SAMPLE_RATE,
@@ -117,32 +149,6 @@ int i2s_init(void)
      	 ESP_LOGE(TAG, "Failed calling i2s_set_pin");
          return rc;
      }
-
-#if 0
-     // Stop now, just to find where the clock problem is....
-     ESP_LOGI(TAG, "Task suspended..."); // FIXME: Remove
-     vTaskSuspend(NULL);
-     return 0; // FIXME: Remove
-#endif
-
-     if ((rc = i2s_set_adc_mode(ADC_UNIT_1, ADC1_CHANNEL_0)) != ESP_OK) {
-     	 ESP_LOGE(TAG, "Failed calling i2s_set_adc_mode");
-         return rc;
-     }
-
-// Does not fix 1-bit size error:  vTaskDelay(5000/portTICK_RATE_MS); i2s_adc_enable(I2S_NUM_0);
-// Does not fix 1-bit size error: i2s_set_sample_rates(I2S_NUM_0, SAMPLE_RATE);
-// Does not fix 1-bit size error: i2s_set_clk(I2S_NUM_0, SAMPLE_RATE, I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_MONO);
-// Does not fix 1-bit size error: adc1_config_width(ADC_WIDTH_BIT_12);
-// Does not fix 1-bit size error: i2s_adc_enable(I2S_NUM_0);
-
-#if 0 // 6086: Does not fix 1-bit size error.
-     // powerup APLL/PLLA
-     SET_PERI_REG_MASK(RTC_CNTL_ANA_CONF_REG, RTC_CNTL_PLLA_FORCE_PU_M);
-     CLEAR_PERI_REG_MASK(RTC_CNTL_ANA_CONF_REG, RTC_CNTL_PLLA_FORCE_PD_M);
-#endif
-
-//     dump_registers();
 
      return ESP_OK;
 }
@@ -203,7 +209,10 @@ esp_err_t app_main(void)
 {
     esp_err_t rc;
     esp_log_level_set("I2S", ESP_LOG_INFO);
-    if ((rc = i2s_init()) != ESP_OK) {
+    if ((rc = i2s_init_0()) != ESP_OK) {
+    	return rc;
+    }
+    if ((rc = i2s_init_1()) != ESP_OK) {
     	return rc;
     }
 
@@ -236,61 +245,61 @@ void tweak_registers()
 	// No change	REG_SET_FIELD(I2S_CONF_REG(0), I2S_TX_MSB_SHIFT, 1);
 }
 
-void dump_registers()
+void dump_registers(int i2s_num)
 {
-    ESP_LOGI(TAG, "I2S_CLKM_CONF_REG(0): %08X", I2S_CLKM_CONF_REG(0));
-    ESP_LOGI(TAG, "I2S_CLKA_ENA(0) = %d",       REG_GET_FIELD(I2S_CLKM_CONF_REG(0), I2S_CLKA_ENA));
-    ESP_LOGI(TAG, "I2S_CLKM_DIV_NUM(0) = %d",   REG_GET_FIELD(I2S_CLKM_CONF_REG(0), I2S_CLKM_DIV_NUM));
-    ESP_LOGI(TAG, "I2S_CLKM_DIV_A(0) = %d",     REG_GET_FIELD(I2S_CLKM_CONF_REG(0), I2S_CLKM_DIV_A));
-    ESP_LOGI(TAG, "I2S_CLKM_DIV_B(0) = %d",     REG_GET_FIELD(I2S_CLKM_CONF_REG(0), I2S_CLKM_DIV_B));
+    ESP_LOGI(TAG, "I2S_CLKM_CONF_REG: %08X", I2S_CLKM_CONF_REG(i2s_num));
+    ESP_LOGI(TAG, "I2S_CLKA_ENA = %d",       REG_GET_FIELD(I2S_CLKM_CONF_REG(i2s_num), I2S_CLKA_ENA));
+    ESP_LOGI(TAG, "I2S_CLKM_DIV_NUM = %d",   REG_GET_FIELD(I2S_CLKM_CONF_REG(i2s_num), I2S_CLKM_DIV_NUM));
+    ESP_LOGI(TAG, "I2S_CLKM_DIV_A = %d",     REG_GET_FIELD(I2S_CLKM_CONF_REG(i2s_num), I2S_CLKM_DIV_A));
+    ESP_LOGI(TAG, "I2S_CLKM_DIV_B = %d",     REG_GET_FIELD(I2S_CLKM_CONF_REG(i2s_num), I2S_CLKM_DIV_B));
 
-    ESP_LOGI(TAG, "I2S_RX_BCK_DIV_NUM(0) = %d", REG_GET_FIELD(I2S_SAMPLE_RATE_CONF_REG(0), I2S_RX_BCK_DIV_NUM));
-    ESP_LOGI(TAG, "I2S_TX_BCK_DIV_NUM(0) = %d", REG_GET_FIELD(I2S_SAMPLE_RATE_CONF_REG(0), I2S_TX_BCK_DIV_NUM));
+    ESP_LOGI(TAG, "I2S_RX_BCK_DIV_NUM = %d", REG_GET_FIELD(I2S_SAMPLE_RATE_CONF_REG(i2s_num), I2S_RX_BCK_DIV_NUM));
+    ESP_LOGI(TAG, "I2S_TX_BCK_DIV_NUM = %d", REG_GET_FIELD(I2S_SAMPLE_RATE_CONF_REG(i2s_num), I2S_TX_BCK_DIV_NUM));
 
-    ESP_LOGI(TAG, "I2S_RX_PCM_BYPASS(0) = %d", REG_GET_FIELD(I2S_CONF1_REG(0), I2S_RX_PCM_BYPASS));
-    ESP_LOGI(TAG, "I2S_TX_PCM_BYPASS(0) = %d", REG_GET_FIELD(I2S_CONF1_REG(0), I2S_TX_PCM_BYPASS));
+    ESP_LOGI(TAG, "I2S_RX_PCM_BYPASS = %d", REG_GET_FIELD(I2S_CONF1_REG(i2s_num), I2S_RX_PCM_BYPASS));
+    ESP_LOGI(TAG, "I2S_TX_PCM_BYPASS = %d", REG_GET_FIELD(I2S_CONF1_REG(i2s_num), I2S_TX_PCM_BYPASS));
 
-    ESP_LOGI(TAG, "I2S_LCD_EN(0) = %d", REG_GET_FIELD(I2S_CONF2_REG(0), I2S_LCD_EN));
-	 ESP_LOGI(TAG, "I2S_CAMERA_EN(0) = %d", REG_GET_FIELD(I2S_CONF2_REG(0), I2S_CAMERA_EN));
-    ESP_LOGI(TAG, "I2S_LCD_TX_SDX2_EN(0) = %d", REG_GET_FIELD(I2S_CONF2_REG(0), I2S_LCD_TX_SDX2_EN));
-    ESP_LOGI(TAG, "I2S_LCD_TX_WRX2_EN(0) = %d", REG_GET_FIELD(I2S_CONF2_REG(0), I2S_LCD_TX_WRX2_EN));
+    ESP_LOGI(TAG, "I2S_LCD_EN = %d", REG_GET_FIELD(I2S_CONF2_REG(i2s_num), I2S_LCD_EN));
+	 ESP_LOGI(TAG, "I2S_CAMERA_EN = %d", REG_GET_FIELD(I2S_CONF2_REG(i2s_num), I2S_CAMERA_EN));
+    ESP_LOGI(TAG, "I2S_LCD_TX_SDX2_EN = %d", REG_GET_FIELD(I2S_CONF2_REG(i2s_num), I2S_LCD_TX_SDX2_EN));
+    ESP_LOGI(TAG, "I2S_LCD_TX_WRX2_EN = %d", REG_GET_FIELD(I2S_CONF2_REG(i2s_num), I2S_LCD_TX_WRX2_EN));
 
 
-    ESP_LOGI(TAG, "I2S_RX_MSB_SHIFT(0) = %d", REG_GET_FIELD(I2S_CONF_REG(0), I2S_RX_MSB_SHIFT));
-    ESP_LOGI(TAG, "I2S_TX_MSB_SHIFT(0) = %d", REG_GET_FIELD(I2S_CONF_REG(0), I2S_TX_MSB_SHIFT));
-    ESP_LOGI(TAG, "I2S_RX_MSB_RIGHT(0) = %d", REG_GET_FIELD(I2S_CONF_REG(0), I2S_RX_MSB_RIGHT));
-    ESP_LOGI(TAG, "I2S_RX_RIGHT_FIRST(0) = %d", REG_GET_FIELD(I2S_CONF_REG(0), I2S_RX_RIGHT_FIRST));
-    ESP_LOGI(TAG, "I2S_RX_SHORT_SYNC(0) = %d", REG_GET_FIELD(I2S_CONF_REG(0), I2S_RX_SHORT_SYNC));
-    ESP_LOGI(TAG, "I2S_TX_SHORT_SYNC(0) = %d", REG_GET_FIELD(I2S_CONF_REG(0), I2S_TX_SHORT_SYNC));
-    ESP_LOGI(TAG, "I2S_RX_SLAVE_MOD(0) = %d", REG_GET_FIELD(I2S_CONF_REG(0), I2S_RX_SLAVE_MOD));
-    ESP_LOGI(TAG, "I2S_TX_SLAVE_MOD(0) = %d", REG_GET_FIELD(I2S_CONF_REG(0), I2S_TX_SLAVE_MOD));
-    ESP_LOGI(TAG, "I2S_TX_START(0) = %d", REG_GET_FIELD(I2S_CONF_REG(0), I2S_TX_START));
-    ESP_LOGI(TAG, "I2S_RX_START(0) = %d", REG_GET_FIELD(I2S_CONF_REG(0), I2S_RX_START));
-    ESP_LOGI(TAG, "I2S_TX_PDM_HP_BYPASS(0) = %d", REG_GET_FIELD(I2S_CONF_REG(0), I2S_TX_PDM_HP_BYPASS));
-    ESP_LOGI(TAG, "I2S_TX_SLAVE_MOD(0) = %d", REG_GET_FIELD(I2S_CONF_REG(0), I2S_TX_SLAVE_MOD));
+    ESP_LOGI(TAG, "I2S_RX_MSB_SHIFT = %d", REG_GET_FIELD(I2S_CONF_REG(i2s_num), I2S_RX_MSB_SHIFT));
+    ESP_LOGI(TAG, "I2S_TX_MSB_SHIFT = %d", REG_GET_FIELD(I2S_CONF_REG(i2s_num), I2S_TX_MSB_SHIFT));
+    ESP_LOGI(TAG, "I2S_RX_MSB_RIGHT = %d", REG_GET_FIELD(I2S_CONF_REG(i2s_num), I2S_RX_MSB_RIGHT));
+    ESP_LOGI(TAG, "I2S_RX_RIGHT_FIRST = %d", REG_GET_FIELD(I2S_CONF_REG(i2s_num), I2S_RX_RIGHT_FIRST));
+    ESP_LOGI(TAG, "I2S_RX_SHORT_SYNC = %d", REG_GET_FIELD(I2S_CONF_REG(i2s_num), I2S_RX_SHORT_SYNC));
+    ESP_LOGI(TAG, "I2S_TX_SHORT_SYNC = %d", REG_GET_FIELD(I2S_CONF_REG(i2s_num), I2S_TX_SHORT_SYNC));
+    ESP_LOGI(TAG, "I2S_RX_SLAVE_MOD = %d", REG_GET_FIELD(I2S_CONF_REG(i2s_num), I2S_RX_SLAVE_MOD));
+    ESP_LOGI(TAG, "I2S_TX_SLAVE_MOD = %d", REG_GET_FIELD(I2S_CONF_REG(i2s_num), I2S_TX_SLAVE_MOD));
+    ESP_LOGI(TAG, "I2S_TX_START = %d", REG_GET_FIELD(I2S_CONF_REG(i2s_num), I2S_TX_START));
+    ESP_LOGI(TAG, "I2S_RX_START = %d", REG_GET_FIELD(I2S_CONF_REG(i2s_num), I2S_RX_START));
+    ESP_LOGI(TAG, "I2S_TX_PDM_HP_BYPASS = %d", REG_GET_FIELD(I2S_CONF_REG(i2s_num), I2S_TX_PDM_HP_BYPASS));
+    ESP_LOGI(TAG, "I2S_TX_SLAVE_MOD = %d", REG_GET_FIELD(I2S_CONF_REG(i2s_num), I2S_TX_SLAVE_MOD));
 
-    ESP_LOGI(TAG, "I2S_RX_FIFO_MOD_FORCE_EN(0) = %d", REG_GET_FIELD(I2S_FIFO_CONF_REG(0), I2S_RX_FIFO_MOD_FORCE_EN));
-    ESP_LOGI(TAG, "I2S_TX_FIFO_MOD_FORCE_EN(0) = %d", REG_GET_FIELD(I2S_FIFO_CONF_REG(0), I2S_TX_FIFO_MOD_FORCE_EN));
-    ESP_LOGI(TAG, "I2S_TX_DATA_NUM(0) = %d", REG_GET_FIELD(I2S_FIFO_CONF_REG(0), I2S_TX_DATA_NUM));
-    ESP_LOGI(TAG, "I2S_RX_DATA_NUM(0) = %d", REG_GET_FIELD(I2S_FIFO_CONF_REG(0), I2S_RX_DATA_NUM));
-    ESP_LOGI(TAG, "I2S_TX_FIFO_MOD(0) = %d", REG_GET_FIELD(I2S_FIFO_CONF_REG(0), I2S_TX_FIFO_MOD));
-    ESP_LOGI(TAG, "I2S_TX_CHAN_MOD(0) = %d", REG_GET_FIELD(I2S_FIFO_CONF_REG(0), I2S_TX_CHAN_MOD));
-    ESP_LOGI(TAG, "I2S_RX_FIFO_MOD(0) = %d", REG_GET_FIELD(I2S_FIFO_CONF_REG(0), I2S_RX_FIFO_MOD));
-    ESP_LOGI(TAG, "I2S_RX_CHAN_MOD(0) = %d", REG_GET_FIELD(I2S_FIFO_CONF_REG(0), I2S_RX_CHAN_MOD));
+    ESP_LOGI(TAG, "I2S_RX_FIFO_MOD_FORCE_EN = %d", REG_GET_FIELD(I2S_FIFO_CONF_REG(i2s_num), I2S_RX_FIFO_MOD_FORCE_EN));
+    ESP_LOGI(TAG, "I2S_TX_FIFO_MOD_FORCE_EN = %d", REG_GET_FIELD(I2S_FIFO_CONF_REG(i2s_num), I2S_TX_FIFO_MOD_FORCE_EN));
+    ESP_LOGI(TAG, "I2S_TX_DATA_NUM = %d", REG_GET_FIELD(I2S_FIFO_CONF_REG(i2s_num), I2S_TX_DATA_NUM));
+    ESP_LOGI(TAG, "I2S_RX_DATA_NUM = %d", REG_GET_FIELD(I2S_FIFO_CONF_REG(i2s_num), I2S_RX_DATA_NUM));
+    ESP_LOGI(TAG, "I2S_TX_FIFO_MOD = %d", REG_GET_FIELD(I2S_FIFO_CONF_REG(i2s_num), I2S_TX_FIFO_MOD));
+    ESP_LOGI(TAG, "I2S_TX_CHAN_MOD = %d", REG_GET_FIELD(I2S_FIFO_CONF_REG(i2s_num), I2S_TX_CHAN_MOD));
+    ESP_LOGI(TAG, "I2S_RX_FIFO_MOD = %d", REG_GET_FIELD(I2S_FIFO_CONF_REG(i2s_num), I2S_RX_FIFO_MOD));
+    ESP_LOGI(TAG, "I2S_RX_CHAN_MOD = %d", REG_GET_FIELD(I2S_FIFO_CONF_REG(i2s_num), I2S_RX_CHAN_MOD));
 
-    ESP_LOGI(TAG, "I2S_TX_PDM_FP(0) = %d", REG_GET_FIELD(I2S_PDM_FREQ_CONF_REG(0), I2S_TX_PDM_FP));
-    ESP_LOGI(TAG, "I2S_TX_PDM_FS(0) = %d", REG_GET_FIELD(I2S_PDM_FREQ_CONF_REG(0), I2S_TX_PDM_FS));
+    ESP_LOGI(TAG, "I2S_TX_PDM_FP = %d", REG_GET_FIELD(I2S_PDM_FREQ_CONF_REG(i2s_num), I2S_TX_PDM_FP));
+    ESP_LOGI(TAG, "I2S_TX_PDM_FS = %d", REG_GET_FIELD(I2S_PDM_FREQ_CONF_REG(i2s_num), I2S_TX_PDM_FS));
 
     	 // Page 317
-    ESP_LOGI(TAG, "I2S_TX_PDM_EN(0) = %d", REG_GET_FIELD(I2S_PDM_CONF_REG(0), I2S_TX_PDM_EN));
-    ESP_LOGI(TAG, "I2S_PCM2PDM_CONV_EN(0) = %d", REG_GET_FIELD(I2S_PDM_CONF_REG(0), I2S_PCM2PDM_CONV_EN));
-    ESP_LOGI(TAG, "I2S_TX_I2S_TX_PDM_SIGMADELTA_IN_SHIFTPDM_EN(0) = %d", REG_GET_FIELD(I2S_PDM_CONF_REG(0), I2S_TX_PDM_SIGMADELTA_IN_SHIFT));
-    ESP_LOGI(TAG, "I2S_TX_PDM_SINC_IN_SHIFT(0) = %d", REG_GET_FIELD(I2S_PDM_CONF_REG(0), I2S_TX_PDM_SINC_IN_SHIFT));
-    ESP_LOGI(TAG, "I2S_TX_PDM_LP_IN_SHIFT(0) = %d", REG_GET_FIELD(I2S_PDM_CONF_REG(0), I2S_TX_PDM_LP_IN_SHIFT));
-    ESP_LOGI(TAG, "I2S_TX_PDM_HP_IN_SHIFT(0) = %d", REG_GET_FIELD(I2S_PDM_CONF_REG(0), I2S_TX_PDM_HP_IN_SHIFT));
-    ESP_LOGI(TAG, "I2S_RX_PDM_SINC_DSR_16_EN(0) = %d", REG_GET_FIELD(I2S_PDM_CONF_REG(0), I2S_RX_PDM_SINC_DSR_16_EN));
+    ESP_LOGI(TAG, "I2S_TX_PDM_EN = %d", REG_GET_FIELD(I2S_PDM_CONF_REG(i2s_num), I2S_TX_PDM_EN));
+    ESP_LOGI(TAG, "I2S_PCM2PDM_CONV_EN = %d", REG_GET_FIELD(I2S_PDM_CONF_REG(i2s_num), I2S_PCM2PDM_CONV_EN));
+    ESP_LOGI(TAG, "I2S_TX_I2S_TX_PDM_SIGMADELTA_IN_SHIFTPDM_EN = %d", REG_GET_FIELD(I2S_PDM_CONF_REG(i2s_num), I2S_TX_PDM_SIGMADELTA_IN_SHIFT));
+    ESP_LOGI(TAG, "I2S_TX_PDM_SINC_IN_SHIFT = %d", REG_GET_FIELD(I2S_PDM_CONF_REG(i2s_num), I2S_TX_PDM_SINC_IN_SHIFT));
+    ESP_LOGI(TAG, "I2S_TX_PDM_LP_IN_SHIFT = %d", REG_GET_FIELD(I2S_PDM_CONF_REG(i2s_num), I2S_TX_PDM_LP_IN_SHIFT));
+    ESP_LOGI(TAG, "I2S_TX_PDM_HP_IN_SHIFT = %d", REG_GET_FIELD(I2S_PDM_CONF_REG(i2s_num), I2S_TX_PDM_HP_IN_SHIFT));
+    ESP_LOGI(TAG, "I2S_RX_PDM_SINC_DSR_16_EN = %d", REG_GET_FIELD(I2S_PDM_CONF_REG(i2s_num), I2S_RX_PDM_SINC_DSR_16_EN));
 
-    ESP_LOGI(TAG, "I2S_TX_CHAN_MOD(0) = %d", REG_GET_FIELD(I2S_CONF_CHAN_REG(0), I2S_TX_CHAN_MOD));
+    ESP_LOGI(TAG, "I2S_TX_CHAN_MOD = %d", REG_GET_FIELD(I2S_CONF_CHAN_REG(i2s_num), I2S_TX_CHAN_MOD));
 
     ESP_LOGI(TAG, "APB_CTRL_SARADC_DATA_TO_I2S = %d", REG_GET_FIELD(APB_CTRL_APB_SARADC_CTRL_REG, APB_CTRL_SARADC_DATA_TO_I2S));
 }
